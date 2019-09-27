5beb679d4c56d933cedfc4ffc42dcc5f32b9a14a
Abhishek Dantale <abhishekdantale@Abhisheks-MacBook-Pro.local>
2019-09-27 14:42:16 +0530

    ver0.17

--- Round2/loginJS.js
+++ Round2/loginJS.js
@@ -1,6 +1,6 @@
 var credentialLoginID = ["Team1","Team2","Team3","Team4","Team5","Team6","Team7","Team8","Team9","Team10","Team11","Team12","Team13","Team14","Team15"]
 var credentialPassword = ["2101","2202","2303","2404","2505","2606","2707","2808","2909","210010","211011","212012","213013","214014","215015"]
-var questionsArray = ["dM_O[(JUTPHT","RLIO%EWO","]M]URVN&YVHG","Hqmiliyi","NI5:!BR228:","Fqprxvht#ufkhpfg#cqf#gqilphgukqi"]
+var questionsArray = ["dM_O[(JUTPHT","RLIO%EWO","]M]URVN&YVHG","aaMQP)XHXPMQI","WGSSCFI","HEOT`NDF","VVBJTV","RPOF","HQMILIYI","NI5:!BR228:","Fqprxvht#ufkhpfg#cqf#gqilphgukqi","FQRMPQPM","dobvtuprn","ndjq!jbwf","oqwmif\"esgsf"]
 
 
 
@@ -14,7 +14,7 @@ function buttonOnClick() {
     if(credentialLoginID.includes(loginID)&&credentialPassword.includes(password)&&(credentialLoginID.indexOf(loginID)===credentialPassword.indexOf(password))){
         console.log("Login")
         localStorage.setItem("loginID", loginID)
-        var index = Math.floor(Math.random()*(6) - 0)
+        var index = Math.floor(Math.random()*(15) - 0)
         console.log("From SetQuestion")
         console.log(questionsArray[index])
         localStorage.setItem("question",questionsArray[index])
