4acb59bdc919a8c040eaf1a01ad26c039004f659
Abhishek Dantale <abhishekdantale@Abhisheks-MacBook-Pro.local>
2019-09-27 11:12:12 +0530

    Ver0.15

--- Round2/loginJS.js
+++ Round2/loginJS.js
@@ -1,6 +1,6 @@
 var credentialLoginID = ["Team1","Team2","Team3","Team4","Team5"]
 var credentialPassword = ["2101","2202","2303","2404","2505"]
-var questionsArray = ["Question1","Question2","Question3","Question4","Question5"]
+var questionsArray = ["dM_O[(JUTPHT","RLIO%EWO","]M]URVN&YVHG","Hqmiliyi","NI5:!BR228:","Fqprxvht#ufkhpfg#cqf#gqilphgukqi"]
 
 
 
@@ -14,7 +14,7 @@ function buttonOnClick() {
     if(credentialLoginID.includes(loginID)&&credentialPassword.includes(password)&&(credentialLoginID.indexOf(loginID)===credentialPassword.indexOf(password))){
         console.log("Login")
         localStorage.setItem("loginID", loginID)
-        var index = Math.floor(Math.random()*(5) - 0)
+        var index = Math.floor(Math.random()*(6) - 0)
         console.log("From SetQuestion")
         console.log(questionsArray[index])
         localStorage.setItem("question",questionsArray[index])
